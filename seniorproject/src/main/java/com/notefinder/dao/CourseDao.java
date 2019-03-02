856e77c599378602c816940273a224121498bfe4
Naomi <surrealisticshadow@gmail.com>
2019-03-01 18:57:04 -0800

    Added functionality to display all courses from the course table in the database. link added to index.jsp

---
+++ seniorproject/src/main/java/com/notefinder/dao/CourseDao.java
@@ -0,0 +1,20 @@
+package com.notefinder.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.notefinder.dao.PostDaoImpl;
+import com.notefinder.models.Course; 
+
+public interface CourseDao {
+	public int save(Course p);
+	public int update(Course p);
+	public int delete(int id);
+	public Course getCourseById(int id);
+	public List<Course> getCourse();
+}
---
+++ seniorproject/src/main/java/com/notefinder/service/CourseManagerImpl.java
@@ -0,0 +1,38 @@
+package com.notefinder.service;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.notefinder.dao.CourseDao;
+import com.notefinder.models.Course;
+
+@Service("courseManager")
+public class CourseManagerImpl implements CourseManager {
+	
+	@Autowired
+	CourseDao dao;
+	
+	public int save(Course p) {
+		return dao.save(p);
+	}
+	
+	public int update(Course p) {
+		return dao.update(p);
+	}
+	
+	public int delete(int id) {
+		return dao.delete(id);
+	}
+	
+	public Course getCourseById(int id) {
+		return dao.getCourseById(id);
+	}
+	
+	public List<Course> getCourse() {
+		return dao.getCourse();
+	}
+
+}
