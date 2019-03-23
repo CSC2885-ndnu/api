72006ad054e3a9cfbd520b69e72bad44e7d06418
meganwong94 <meganwong94@gmail.com>
2019-03-22 19:56:18 -0700

    changed getPostbyId to List<post>

--- seniorproject/src/main/java/com/notefinder/service/PostManagerImpl.java
+++ seniorproject/src/main/java/com/notefinder/service/PostManagerImpl.java
@@ -2,7 +2,6 @@ package com.notefinder.service;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,7 +26,7 @@ public class PostManagerImpl implements PostManager {
 		return dao.delete(id);
 	}
 	
-	public Post getPostById(int id) {
+	public List<Post> getPostById(int id) {
 		return dao.getPostById(id);
 	}
 	
